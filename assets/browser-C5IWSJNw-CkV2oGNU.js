29fe265c84fed31d6a36a96d3650e952b54e699f
Mimi Lertsaroj <mimi.lertsaroj.3@gmail.com>
2025-09-24 16:10:13 -0500

    Updates

--- assets/browser-C5IWSJNw-CkV2oGNU.js
+++
@@ -1,1 +0,0 @@
-import{A as e}from"./index-CR0mkU9Z.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const t=e(o),n=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"}));export{n as b};
